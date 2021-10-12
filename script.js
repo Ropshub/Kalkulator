92ef471cb3ca426fdde42ac41bfde02930892ed5
Ropshub <84778312+Ropshub@users.noreply.github.com>
2021-10-12 15:22:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 document.querySelector(".plus").onclick=function(){
 	let n1=document.querySelector(".number1").value;
 	let n2=document.querySelector(".number2").value;
-	alert(parseInt(n1)+parseInt(n2))
-	querySelector(".number1").value = "";
-	querySelector(".number2").value = "";
+	alert(parseInt(n1)+parseInt(n2));
+	document.querySelector(".number1").value = "";
+	document.querySelector(".number2").value = "";
 }
 document.querySelector(".minus").onclick=function(){
 	let n1=document.querySelector(".number1").value;
 	let n2=document.querySelector(".number2").value;
-	alert(parseInt(n1)-parseInt(n2))
-	querySelector(".number1").value = "";
-	querySelector(".number2").value = "";
+	alert(parseInt(n1)-parseInt(n2));
+	document.querySelector(".number1").value = "";
+	document.querySelector(".number2").value = "";
 }
 document.querySelector(".umn").onclick=function(){
 	let n1=document.querySelector(".number1").value;
 	let n2=document.querySelector(".number2").value;
-	alert(parseInt(n1)*parseInt(n2))
-	querySelector(".number1").value = "";
-	querySelector(".number2").value = "";
+	alert(parseInt(n1)*parseInt(n2));
+	document.querySelector(".number1").value = "";
+	document.querySelector(".number2").value = "";
 }
 document.querySelector(".del").onclick=function(){
 	let n1=document.querySelector(".number1").value;
 	let n2=document.querySelector(".number2").value;
-	alert(parseInt(n1)/parseInt(n2))
-	querySelector(".number1").value = "";
-	querySelector(".number2").value = "";
+	alert(parseInt(n1)/parseInt(n2));
+	document.querySelector(".number1").value = "";
+	document.querySelector(".number2").value = "";
 }
